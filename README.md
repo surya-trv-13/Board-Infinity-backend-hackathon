88998ff4cd85d38752eabc4c00dcd5d5a2573f93
suryanarayan.rath <suryanarayan.rath@highradius.com>
2020-09-07 00:37:52 +0530

    Created Schema for 📝and Setup the MongoDB 🍃

---
+++ README.md
@@ -0,0 +1,9 @@
+# BROAD INFINITY BACKEND HACKATHON
+
+## 06-September-2020
+
+### Created the project ,installed specific NPM packages for the project
+
+### Connected the project with the MONGO DB Atlas
+
+### Created Schema for the TODO using mongoose and validated using validator
---
+++ model/todo.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+var Schema = mongoose.Schema;
+
+var todoSchema = new Schema({
+  taskName: {
+    required: true,
+    type: String,
+    minlength: 5,
+    trim: true,
+  },
+  taskDescription: {
+    type: String,
+  },
+  creator: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Number,
+    required: true,
+  },
+});
+
+var Todo = mongoose.model("Todo", todoSchema);
+
+module.exports = { Todo };
